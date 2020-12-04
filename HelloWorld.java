c568aa81f50ef3f15d59711ddb6581db32332b5c
Genilson Cavalcante de Oliveira <73254486+GenilsonCavalcante@users.noreply.github.com>
2020-12-04 15:45:47 -0300

    Testes no ambiente de desenvolvimento Intellij

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+public class HelloWorld {
+    public static void main(String[] args){
+
+        System.out.println("Hello Mundo");
+
+        //Atalhos do Intellij:
+
+        /*
+        CTRL + ESPAÇO - Completa códigos
+        ALT + ENTER - Mostra uma lista de ações para correção de código
+        CTRL + N - Busca uma classe pelo nome
+        Shift - Duas vezes, busca qualquer arquivo
+
+        ALT + 1 - Minimiza ou Maximiza a tela de projetos
+        sout - System.out.println();
+        CTRL + D - Duplica a linha de código que o cursor está
+        CTRL + SHIFT + F10 - Cria o atalho de execução de código, na barra a cima
+        SHIFT + F10 - Após de criado o atalho de executação, é so executar os códigos com SHIFT + F10
+        CTRL + ALT + SETA PARA DIREITA ou SETA PARA ESQUERDA - Faz com que você mude de classe para a outra
+        ALT + INSERT - Faz com que você crie um construtor
+        CTRL + ALT + F - Clicando o cursor em cima de uma variável, e apertando esse atalho, ele vai cria um atributo relacionada a variável selecioda
+        CTRL + ALT + L - Esse atalho faz organizar todo o código mal endentado
+        CTRL + B - Faz com que ao clicar na variável, ela ,ostra a origem onde foi declarada no código
+
+         */
+    }
+}
---
+++ Testando.java
@@ -0,0 +1,8 @@
+public class Testando {
+
+    private Integer idade;
+
+    public Testando(Integer idade) {
+        this.idade = idade;
+    }
+}
