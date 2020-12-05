8db04109f7ed5f2419611279d6364c181a597235
Genilson Cavalcante de Oliveira <73254486+GenilsonCavalcante@users.noreply.github.com>
2020-12-05 10:40:28 -0300

    Introdução A POO(Programação-Orientada-a-Objetos)

---
+++ POO-Java/Aula001/src/IntroducaoAPoo.java
@@ -0,0 +1,22 @@
+public class IntroducaoAPoo {
+    public static void main(String[] args) {
+        //PROGRAMAÇÃO-ORIENTADA-A-OBJETOS
+
+        //POO - Paradigma de programação utilizado no desenvolvimento de projetos de software.
+        /*
+        Esses paradigmas utilizam padrões para a arquitetura da programação,
+        Essa orientação a objetos é a forma de abstração da programação com
+        os objetos da vida real, fazendo esse paralelo e menipulando os mesmos.
+
+        Design Pattern - Padrões de projetos para soluções recorrentes.
+
+        Um dos maiores motivos para se conhecer da Programação Orientada a Objetos
+        é que a mesma trabalha com os conceitos de modularização, padronização,
+        fazendo com que os seus códigos fiquem mais organizados e possam ser reaproveitados
+        em tantos outros projetos. Moldando as bibliotecas que serão tilizadas.
+
+        Os objetos serão a materialização de nossas idéias, fazendo que os nossos sonhos
+        se tornem parte fisicamente de nossa realidade todos os dias.
+         */
+    }
+}
---
+++ POO-Java/Aula002/src/ContinuacaoAIntroducaoPOO.java
@@ -0,0 +1,14 @@
+public class ContinuacaoAIntroducaoPOO {
+    public static void main(String[] args) {
+        /*
+        PROGRAMAÇÃO ORIENTADO A OBJETOS
+
+        É um paradígma de programação onde tudo que é representado como se tudo fosse um objeto físico.
+        Paradigma de programação que define entidades como sendo objetos.
+
+        Em um objeto temos os ATRIBUTOS e FUNÇÕES.
+
+        objetos possuem a sua responsabilidade  de manter oculto as suas informações.
+        */
+    }
+}
