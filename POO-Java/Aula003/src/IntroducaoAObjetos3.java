8dc0ba58d45ae2451ef0e3c31776f40675497611
Genilson Cavalcante de Oliveira <73254486+GenilsonCavalcante@users.noreply.github.com>
2020-12-05 16:57:04 -0300

    Add Aula003 do curso de POO

---
+++ POO-Java/Aula003/src/IntroducaoAObjetos3.java
@@ -0,0 +1,39 @@
+public class IntroducaoAObjetos3 {
+    public static void main(String[] args) {
+        /*
+        Segundo o sicionário aurélio, Objeto é tudo que é
+        manipulado ou então, que pode ser. Um objeto possui
+        características e pode desempenhar alguma função.
+
+        Atributo ou então, propiedade, é tudo que diz respeito
+        a um objeto.
+
+        Função, ou então, método, compreende tudo o que um
+        objeto faz.
+
+        De forma resumida, podemos dizer que a Orientação a
+        Objetos na Programção possui quatro pilares:
+
+        - ENCAPSULAMENTO: É a concentração de muitas informações
+        em apenas um único local, encapsuladdo. Podemos dizer que
+        um grupo de informações estção encapsuladas em um objeto.
+
+        - CLASSE: É a que define a estrutura de um objeto, é o projeto
+        de todo o objeto
+
+        - OBJETO: Todo objeto na programação foi criado por uma classe,
+        ou seja, de uma nomatização. Se temos que a classe é o projeto,
+        o objeto é a execução da classe, ou seja, é a materialização do
+        projeto.
+
+        - HERANÇA: É a capacidade que temos de programar herdando características
+        de seus ancestrais. Podemos definir uma classe baseando em outra.
+
+        OBS: Devemos entender que dentro de um objeto, podemos ter vários
+        projetos. Pela união desses e troca de informações na comunicação
+        entre eles, temos um novo objeto. Sendo que em um objeto maior que
+        possui nele outro sub-objetos, sendo estes os atributos desse objeto
+        maior.
+         */
+    }
+}
