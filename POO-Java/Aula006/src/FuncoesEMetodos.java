acb24754b6a7cab716744ec635a9e3c11cfb0489
Genilson Cavalcante de Oliveira <73254486+GenilsonCavalcante@users.noreply.github.com>
2020-12-06 09:59:28 -0300

    Adicionado arquivo Aula006 - Onde fala sobre métodos

---
+++ POO-Java/Aula006/src/FuncoesEMetodos.java
@@ -0,0 +1,17 @@
+public class FuncoesEMetodos {
+
+    public static void metodo(){
+        //OBS: Quando vemos que estamos trabalhando com uma declaração de variável em seguida de parênteses, concerteza estmaos trabalhando com métodos.
+        System.out.println("Impresso atraves do método.");
+    }
+
+    public static void main(String[] args) {
+        /*
+        MODELO DE UMA FUNÇÂO:
+        [modificador] [tipo] nome(){
+            //corpo();
+        };
+         */
+        metodo();//Chamando o nosso método!
+    }
+}
