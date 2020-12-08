3a18884f9c9a4628bd32697733d0d1ae91a7aefc
Genilson Cavalcante de Oliveira <cavalcantedeoliveiragenilson@gmail.com>
2020-12-08 16:36:46 -0300

    Iniciei o Curso de POO-Java do Curso em Vídeo

---
+++ POO-Java_CursoEmVideo/Aula001/src/OQueEProgramacaoOrientadaAObjeto.java
@@ -0,0 +1,67 @@
+public class OQueEProgramacaoOrientadaAObjeto {
+    public static void main(String[] args){
+
+          /*
+          ORIGEM DO POO: Na origens da programação, o seu início se dá
+          pela programação de baíxo nível, nas décadas de 50 e 60, onde
+          os códigos digitados eram os mesmos interpletaddos pela máquina,
+          linguagens binárias, decimais... Mas adiante, teve-se uma pequena
+          evolução com a programação linear, estruturas de comandos ordenados
+          um após o outro, continuando, teve-se um avanço mais significativo na
+          programação estruturada, onde a construção dos comandos eram mais
+          significativos e poderiam seguir uma lógica mesmo estando em lugares
+          não sequenciais do código. Adiante, com a programação modular, é a
+          composição dos códigos encapsulados em partes, dando mais estruturação
+          ao código, mas a vida desse tipo de programação tive a sua vida reduzida,
+          pois se deu origem a programção orientada a objetos, trazendo conceitos
+          mais avançados e muitas vantagens a dessa última.
+
+          Alan Kay - Um cara que interconectou os conceitos de matemática e
+          biologia para criar a programação.
+
+
+
+          OBJETIVO DO POO: Aproximar o mundo digital do
+          mundo real;
+
+
+          -POO-
+          Vantagens: 'COMERNada'
+
+          Confiável: O isolamento entre as partes gera sogtware seguro. Ao alterar
+          uma parte, nenhuma outra é afetada.
+          EX: Se pegar um controle remote, temos que este é um objeto e dentro dele
+          vemos outro objeto, pilha que interage com o mesmo, logo se retirarmos um
+          pilha do controle e trocarmos por outra de uma marca diferente, o "software"
+          não deixará de funcionar, essa é um conceito de confiabilidade do POO.
+
+          Oportuno: Ao dividir tudo em partes, várias delas podem ser
+          desenvildidas em parelelo.
+          EX: Eu não preciso desenvolver primeito o controle remoto para em seguida
+          desenvolver a pilha, posso fazer isso simultaneamente e separadamente e executar.
+
+          Manutenível: Atualizar um software é mais fácil. Uma pequena modificação vai
+          beneficiar toas as partes que usarem o objeto.
+          EX: Quando eu troco uma pilha velha do meu controle por uma pilha nova, logo não
+          preciso alterar nada. Esse é o processo de manutenção de sóftware.
+
+          Extensível: O software não é estático. Ele deve crescer par permanecer útil;
+          EX: Eu não preciso criar um novo controle para adicionar novas funcionalidades,
+          apenas pego o que já foi criado e faço as devidas alterações.
+
+          Reutilizável: Podemos usar objetos de um sistema que criamos em outro sistema futuro.
+          EX: Se eu tenho um controle que funciona em uma tv, mas eu troco a tv e que a mesma
+          possui compatibilidade com esse controle, eu posso utilizá-lo.
+
+          Natural: Mais fácil de entender. Você se preocupa mais na funcionalidade do que nos
+          detalhes de implementação.
+
+
+
+
+
+           */
+
+    }
+
+}
