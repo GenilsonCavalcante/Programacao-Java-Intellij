dd62172d6c1286568c8ee0beb37f34e837152a3f
Genilson Cavalcante de Oliveira <cavalcantedeoliveiragenilson@gmail.com>
2020-12-18 18:24:16 -0300

    Aula006 - Encapsulamento - Pilares da Programação

---
+++ POO-Java_CursoEmVideo/Aula006a/src/PilaresDaPOO_Encapsulamento.java
@@ -0,0 +1,53 @@
+public class PilaresDaPOO_Encapsulamento {
+    public static void main(String[] args){
+
+        //Pilares da POO.
+        /*
+            3 pilares do POO:
+
+            E - ENCAPSULAMENTO. - lembra de cápsula
+            H - HERANÇA. - lembra da herança de seu avó
+            P - POLIMORFISMO. - 'poli' de muito e 'morfismo' de forma, ou seja, muitas formas de se fazer.
+
+            ENCAPSULAMENTO:
+            Tem a idéia de encapsular, criar uma cápsula, que dê proteção
+            as informações contidas nesta. Um exemplo é uma pilha, onde que dentoro dela
+            existe vários componentes que podem prejudicar ao humano e o humano em contato
+            com esses componente pode prejudicar a pilha no seu desempenho. Por isso é que
+            a pilha possui uma cápsula, um envoltório que impede esse acesso do usuário com
+            algumas coisas internas da pilha.
+
+            Além do encapsulamento fornecer essa proteção, ela também possui putra vantagem
+            que é trazer a padronização dos elementos criados. Exemplo, um modelo de pilha,
+            pode ser utilizado por qualquer controle remoto.
+
+            Conclui-se que o encapsulamento protege o código do usuário/programador e empede
+            que ele fassa besteira, assim como cria uma pafronização para construção de códigos.
+
+            Encapsular - Ocultar partes independentes da implementaçãp, permitindo construir
+            partes invisíveis ao mundo exterior.
+
+            Podemos ainda trocar mensagens no mundo extor com o mundo interno dessa cápsula,
+            sem haver a necessidade real de mexermos ou comunicamos diretamente com o interior
+            dessa cápsula.
+
+            Esse canal de comunicação externa possui o nome na POO de INTERFACE.
+            Um bom objeto encapsulado possui uma interface bem definida.
+
+            INTERFACE:  Lista de serviços fornecidos por um componente. É o contato com o
+            mundo exterior, que define o que pode ser feito com um objeto dessa classe.
+
+            OBS: ENPAPSULAR NÃO É OBRIGATÓRIO, MAS É UMA PRÁTICA PARA PRODUZIR CLASSES MAIS EFICIENTES.
+            OS PILARES DA PROGRAMAÇÃO SUSTENTAM OS PARÂMETROS TRABALHADOS, POR MAIS QUE NÃO SEJE
+            OBRIGATÓRIO USÁ-LOS, MAS DÁ AO CÓDIGO MAIS SIGNIFICADO, EVITA DE POSSÍVEIS ERROS NO FUTURO.
+
+            OBS: Quando vamos encapsular um objeto, tornamos todos os seus atributos privados/protegidos
+            isso, para dar a proteção aos códigos.
+
+            métodos abstratos significa que não vou desenvolver o código dentro de onde o mesmo foi declarado.
+            ou seja, na interface eu não digo o que os métodos vão fazer, apenas vou utilizalos.
+
+         */
+
+    }
+}
