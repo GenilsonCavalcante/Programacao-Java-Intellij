fd9e3dde8a8edc3e1752723a7a23149157a91a3e
Genilson Cavalcante de Oliveira <cavalcantedeoliveiragenilson@gmail.com>
2020-12-19 17:11:01 -0300

    Atalhos úteis para navegar no Intellij IDEA

---
+++ AtalhosIntellij.java
@@ -0,0 +1,28 @@
+public class AtalhosIntellij {
+    public static void main(String[] args){
+
+        System.out.println("Hello Mundo");
+
+        //Atalhos do Intellij:
+
+        /*
+        CTRL + ESPAÇO - Completa códigos
+        ALT + ENTER - Mostra uma lista de ações para correção de código
+        CTRL + N - Busca uma classe pelo nome
+        Shift - Duas vezes, busca qualquer arquivo
+
+        ALT + 1 - Minimiza ou Maximiza a tela de projetos
+        sout - System.out.println();
+        CTRL + D - Duplica a linha de código que o cursor está
+        CTRL + SHIFT + F10 - Cria o atalho de execução de código, na barra a cima
+        SHIFT + F10 - Após de criado o atalho de executação, é so executar os códigos com SHIFT + F10
+        CTRL + ALT + SETA PARA DIREITA ou SETA PARA ESQUERDA - Faz com que você mude de classe para a outra
+        ALT + INSERT - Faz com que você crie um construtor, ou mpetodos especiais falcilmente, ex: métodos getters e setters.
+        CTRL + ALT + F - Clicando o cursor em cima de uma variável, e apertando esse atalho, ele vai cria um atributo relacionada a variável selecioda
+        CTRL + ALT + L - Esse atalho faz organizar todo o código mal endentado
+        CTRL + B - Faz com que ao clicar na variável, ela ,ostra a origem onde foi declarada no código
+        ALT + SHIFT + INSERT - Faz com que após selecionar um código, ao digitar qualquer coisa, o que estiver sendo digitado refletirá e escreverá nas linhas selecionadas as mesmas coisas. Para tirar do modo coluna é só digitar o mesmo atalho e volta tudo ao normal.
+
+         */
+    }
+}
