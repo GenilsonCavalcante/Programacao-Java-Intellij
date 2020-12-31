94f9104b7394e84fad8c4bd3a66617034f4ba99c
Genilson Cavalcante de Oliveira <cavalcantedeoliveiragenilson@gmail.com>
2020-12-31 16:42:07 -0300

    Add Aula012a - Conceito Polimorfismo

---
+++ POO-Java_CursoEmVideo/Aula012a/src/ConceitoPolimorfismo.java
@@ -0,0 +1,38 @@
+public class ConceitoPolimorfismo {
+    public static void main(String[] args){
+
+        //POLIMORFISMO: Último pilar da POO.
+        /*
+            POLI --> muitas; MORFO --> formas; muitas formas.
+            EX: Todos os dias eu acordo, acordar seria para a programação
+            uma função, executada todos os dias. Na realidade é que de segunda
+            a sexta eu acordo cedo para o trabalho, mas nos sábados e domingos,
+            eu acordo um pouco mais tarde, pois nesses dias eu não tenho trabalho.
+            Percebe-se as diversas formas de acordar durante a semana, isso é um
+            exemplo de polimorfismo.
+            conceito mais formal:
+
+            POLIMORFISMO: Permite que um mesmo nome represente vários comportamentos
+            diferentes.
+
+            ASSINATURA DO MÉTODO: Quantidade e os tipos dos parâmetros.
+            EXEMPLOS:
+            public void calcMedia(float n1, float n2){Retorna um valor do tipo Real}
+            public void calcMedia(float v1, float v2){Retorna um valor do tipo Inteiro}
+            OBS: Como os dois métodos atendem as mesmas quantidades e os tipos dos parâmetros, eles possuem a mesma ASSINATURA DO MÉTODO, mesmo retornando tipos de valores diferentes.
+
+            public void calcMedia(int bim, float n1, float n2){Retorna um valor do tipo Real}
+            - Note que esse último não possui a mesma ASSINATURA DO MÉTODO, pois não atende a definição.
+
+            TIPOS DE POLIMORFISMO:
+            OBS: São ao todo quatro tipos, mas no curso em vídeo o gustavo considerou apenas dois;
+
+            1° POLIMORFISMO DE SOBREPOSIÇÃO: Acontece quando substituímos um método de uma superclasse na sua subclasse, usando a mesma assinatura.
+
+            2° POLIMORFISMO DE SOBRECARGA:
+
+
+         */
+
+    }
+}
