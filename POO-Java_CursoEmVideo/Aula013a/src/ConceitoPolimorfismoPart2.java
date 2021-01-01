088271b853a214874c72065c55e5577b5896d799
Genilson Cavalcante de Oliveira <cavalcantedeoliveiragenilson@gmail.com>
2021-01-01 17:46:54 -0300

    Add Aula013a - Conceito sobre Polimorfismo part.2

---
+++ POO-Java_CursoEmVideo/Aula013a/src/ConceitoPolimorfismoPart2.java
@@ -0,0 +1,16 @@
+public class ConceitoPolimorfismoPart2 {
+    public static void main(String[] args){
+
+        //TIPOS DE POLIMORFISMO:
+        /*
+            1° SOBREPOSIÇÃO: Métodos que possuem a mesma assinatura pode ser implementados, sobrepostos em métodos diferentes. OBS: A assinatura é a mesma quantidade e tipo dos atributos; sobreposição: @Override;
+            2° SOBRECARGA: São métodos do mesmo nome, assinaturas diferentes e dentro de uma mesma classe. Portanto, esse é o tipo de polimorfismo de sobrecarga;
+         PARA EXEMPLIFICAR MELHOR:
+
+            POLIMORFISMO DE SOBREPOSIÇÃO: Mesma Assinatura e em Classes DIferentes;
+            POLIMORFISMO DE SOBRECARGA: Assinaturas Diferentes e na Mesma Classe;
+
+         */
+
+    }
+}
